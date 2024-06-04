541eb56a3bc0ba1f783292c812bf2467c5a255c0
Abhishek <abhishekmech95@gmail.com>
2024-06-04 09:06:31 +0530

    kafka consumer added

---
+++ kafka/kafka-consumer/src/main/java/com/technoelavte/kafka_consumer/consumer/KafkaMessageListener.java
@@ -0,0 +1,27 @@
+package com.technoelavte.kafka_consumer.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaMessageListener {
+
+    Logger log = LoggerFactory.getLogger(KafkaMessageListener.class);
+
+    @KafkaListener(topics = "topic-1", groupId = "topic-group")
+    public void consumer1(String message) {
+        log.info("consumer-1 consume the message {}", message);
+    }
+
+    @KafkaListener(topics = "topic-1", groupId = "topic-group")
+    public void consumer2(String message) {
+        log.info("consumer-2 consume the message {}", message);
+    }
+
+    @KafkaListener(topics = "topic-1", groupId = "topic-group")
+    public void consumer3(String message) {
+        log.info("consumer-2 consume the message {}", message);
+    }
+}
